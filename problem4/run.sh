559875e9cee2bb34ebb5e7b035354e8f6cce2617
Yang Hong <yh013@bucknell.edu>
2023-02-12 15:45:50 -0500

    progress in problem 4

---
+++ problem4/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "" && echo "AdaptedConvergecast tests:"
+echo "" && echo "Using function max:"
+mpiexec -n 15 python adapted_convergecast.py --test ../tests/rooted_tree0.txt --func max
+echo "" 
+mpiexec -n 11 python adapted_convergecast.py --test ../tests/rooted_tree1.txt --func max
+
+echo "" && echo "Using function min:"
+mpiexec -n 15 python adapted_convergecast.py --test ../tests/rooted_tree0.txt --func min
+echo "" 
+mpiexec -n 11 python adapted_convergecast.py --test ../tests/rooted_tree1.txt --func min
+
+echo "" && echo "Using function sum:"
+mpiexec -n 15 python adapted_convergecast.py --test ../tests/rooted_tree0.txt --func sum
+echo "" 
+mpiexec -n 11 python adapted_convergecast.py --test ../tests/rooted_tree1.txt --func sum
+
+echo "" && echo "Using invalid functions:"
+mpiexec -n 15 python adapted_convergecast.py --test ../tests/rooted_tree0.txt --func print
