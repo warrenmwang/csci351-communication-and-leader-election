0ccbff204d6398ac92d0b2c1db738bc123a4aa9e
jcc045 <jcc045@bucknell.edu>
2023-02-16 18:32:32 -0500

    message count at +2

--- problem2/run.sh
+++ problem2/run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 echo "problem 2 tests go here:"
 echo "unidirectional LE ring test"
-echo "" 
+echo ""
 mpiexec -n 7 python unidirectionalLE.py --test ../tests/ringcopy.txt
 
-echo "" 
+echo ""
 
 python3 RandomRings.py --size 10
 mpiexec -n 11 python unidirectionalLE.py --test ../tests/randomRing.txt
 
-echo "" 
+echo ""
 
 mpiexec -n 7 python bidirectionalLE.py --test ../tests/ringcopy.txt
+
+echo ""
+
+python3 RandomRings.py --size 10
+mpiexec -n 11 python bidirectionalLE.py --test ../tests/randomRing.txt
+
