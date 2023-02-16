992cab780d2dc50f4116e0d217d9d890eb60ea0a
Andrew J Passero <ajp031@linuxremote-csci315.bucknell.edu>
2023-02-15 22:15:40 -0500

    Workaround for ounting total messages sent

--- problem2/run.sh
+++ problem2/run.sh
@@ -2,10 +2,10 @@
 echo "problem 2 tests go here:"
 echo "unidirectional LE ring test"
 echo "" 
-mpiexec -n 6 python unidirectionalLE.py --test ../tests/ringcopy.txt
+mpiexec -n 7 python unidirectionalLE.py --test ../tests/ringcopy.txt
 
 echo "" 
 
 python3 RandomRings.py --size 10
-mpiexec -n 10 python unidirectionalLE.py --test ../tests/randomRing.txt
+mpiexec -n 11 python unidirectionalLE.py --test ../tests/randomRing.txt
 
--- problem2/unidirectionalLE.py
+++ problem2/unidirectionalLE.py
@@ -15,48 +15,74 @@ testfile = args.test
 #algorithm always sends to the left neighbor. 
 
 comm = MPI.COMM_WORLD 
-
+size = comm.Get_size()-1
 rank = comm.Get_rank() 
 
-neighbors = read_graph(testfile,rank)
-##The left neighbor is always neighbors[0]
-
-
-
-terminateNonLeader = False
-comm.isend((rank,terminateNonLeader), dest = neighbors[0])
-#Initially send a message that is of type tuple where tuple = (rank,terminateNonLeader) and terminateAsNon Leader is bool 
-#flagging if the message being sent should have the receiving process terminate 
-
-
-
-while True: 
-    recvRequest = comm.irecv()
+if rank == size: 
+    countMessagesRecieved = 0
+    totalMessages = 0 
     while True: 
-        recvData = recvRequest.test()
-        if recvData[0]:
-            break 
+        recvRequest = comm.irecv() 
+        while True: 
+            recvData = recvRequest.test()
+            if recvData[0]:
+                break
+        totalMessages += recvData[1]
+        countMessagesRecieved += 1
+        if countMessagesRecieved == size: 
+            print(f"Total number of messages is {totalMessages} ")
+            sys.exit()
+    
+        
+else: 
+    neighbors = read_graph(testfile,rank)
+    ##The left neighbor is always neighbors[0]
 
-    ##ID received is index 1 of recvData and index 0 of the tuple
-    recvID = recvData[1][0]
-    terminateNonLeader = recvData[1][1]
-    # print(f"process {rank} received id: {recvID}")
-    
-    if terminateNonLeader: 
-        #if terminateNonLeader is true pass message on and terminate 
-        print(f"process {rank} terminated NONLEADER")
-        comm.isend((rank,True), dest = neighbors[0])
-        sys.exit()
-    if recvID != rank: 
-        #If receiving ID != rank 
-        if recvID > rank: 
-            #Pass received ID to left process if it is greater than own ID 
-            # print(f"{rank} Passing {recvID} to {neighbors[0]}")
-            comm.isend((recvID,False), dest = neighbors[0])
-    elif recvID == rank: 
-        #If recvID == rank, this process is leader, send message to left node with terminateAsNonleader = true, and terminate
-        leader = True
-        print(f"process {rank} has terminated as LEADER")
-        comm.isend((rank,True), dest = neighbors[0])
-        sys.exit() 
-    
+    leader = False
+
+    numMessages = 0
+    terminateNonLeader = False
+    comm.isend((rank,terminateNonLeader), dest = neighbors[0])
+    numMessages+=1 
+    #Initially send a message that is of type tuple where tuple = (rank,terminateNonLeader) and terminateAsNon Leader is bool 
+    #flagging if the message being sent should have the receiving process terminate 
+    while True: 
+        recvRequest = comm.irecv()
+        while True: 
+            recvData = recvRequest.test()
+            if recvData[0]:
+                break 
+
+        ##ID received is index 1 of recvData and index 0 of the tuple
+        recvID = recvData[1][0]
+        terminateNonLeader = recvData[1][1]
+        # print(f"process {rank} received id: {recvID}")
+        
+        if terminateNonLeader: 
+            #if terminateNonLeader is true pass message on and terminate 
+            
+            comm.isend((recvID,True), dest = neighbors[0])
+            numMessages+=1
+            comm.isend(numMessages,dest = size)
+            print(f"process {rank} terminated NONLEADER")
+            
+            sys.exit()
+        if recvID != rank: 
+            #If receiving ID != rank 
+            if recvID > rank: 
+                #Pass received ID to left process if it is greater than own ID 
+                # print(f"{rank} Passing {recvID} to {neighbors[0]}")
+                comm.isend((recvID,False), dest = neighbors[0])
+                numMessages+=1
+        elif recvID == rank: 
+            #If recvID == rank, this process is leader, send message to left node with terminateAsNonleader = true, and terminate
+            leader = True
+            print(f"process {rank} has terminated as LEADER")
+            comm.isend(numMessages,dest = size)
+            comm.isend((rank,True), dest = neighbors[0])
+            numMessages+=1
+            sys.exit() 
+
+
+
+
